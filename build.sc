8ec33f7ae973831bc3d7f52be2b1cde12af6ec74
Georg Ofenbeck - taaofge1 <georg.ofenbeck@swisscom.com>
2024-04-16 17:54:40 +0000

    jupyter notebook

--- build.sc
+++ build.sc
@@ -3,12 +3,13 @@ import mill.scalalib._
 
 object namefinder extends ScalaModule {
 
-  def scalaVersion = "3.3.3"
+  def scalaVersion = "2.13.13"
   def ammoniteVersion = "3.0.0-M1"
   def ivyDeps = Agg(
     ivy"com.github.tototoshi::scala-csv:1.3.10",
     ivy"com.lihaoyi::os-lib:0.10.0",
-    ivy"io.github.pityka::nspl-awt:0.10.0"
+    ivy"io.github.pityka::nspl-awt:0.10.0",
+    ivy"org.plotly-scala::plotly-almond:0.8.4",
     )
   object test extends ScalaTests with TestModule.Munit {
     def ivyDeps = Agg(
--- namefinder/src/com/ofenbeck/Hello.scala
+++ namefinder/src/com/ofenbeck/Hello.scala
@@ -7,6 +7,7 @@ import java.io.File
 
 import os._
 import com.ofenbeck.Namefinder.NameStats
+import plotly._, plotly.element._, plotly.layout._, plotly.Almond._
 
 object Namefinder extends App {
 
@@ -26,13 +27,13 @@ object Namefinder extends App {
   }
   val mapped = classed
     .groupBy(_.name)
-    .map((name, femname) =>
+    .map { case (name, femname) =>
       val total = femname.map(_.value).sum
       val last10years = femname.filter(_.yearOfBirth > 2010).map(_.value).sum
       val lastYear = femname.filter(_.yearOfBirth == 2022).map(_.value).sum
       val syllables = countSyllables(name)
       NameStats(name, total, last10years, syllables, lastYear)
-    )
+    }
     .toVector
     .view
     .filter(_.syllables <= 3)
@@ -43,56 +44,61 @@ object Namefinder extends App {
     // .filter(_.total > 50)
     // sortBy(_.total) { Ordering[Int].reverse }
     .sortBy(_.lastYear)(Ordering[Int].reverse)
-    .drop(200)
-    .take(10)
-  println(s"Down to ${mapped.length}")
-  mapped.foreach(x => println(x))
+  // .drop(200)
+  // .take(10)
+  val vecs = mapped.zipWithIndex.foldLeft((Vector.empty[Int],Vector.empty[Int])) { case (acc, ele) =>
+    ele match {
+      case (nameStats, index) =>
+      (acc._1 :+ index, acc._2 :+ nameStats.lastYear)
+    }
+  }
+  val (xv,yv) = vecs
+  //println(s"Down to ${mapped.length}")
+  //mapped.foreach(x => println(x))
 
   // classed.foreach(println)
 // decodet.foreach(println)
 
   // Process the CSV lines here
 
-}
-
-
-def csvToClass(reader: CSVReader): List[FemaleName] = {
-  val withHeader = reader.allWithHeaders()
-  val classed = withHeader.map { row =>
-    // print(row.keySet)
-    if (
-      row.keySet != Set(
-        "TIME_PERIOD",
-        "firstname",
-        "YEAROFBIRTH",
-        "VALUE",
-        "OBS_STATUS"
-      )
-    ) {
-      // println("Error: CSV file does not have the expected header")
-      // println(row.keySet)
-      FemaleName("", "", 0, 0, "")
-    } else
-      FemaleName(
-        row("TIME_PERIOD"),
-        row("firstname"),
-        row("YEAROFBIRTH").toInt,
-        row("VALUE").toInt,
-        row("OBS_STATUS")
-      )
+  def csvToClass(reader: CSVReader): List[FemaleName] = {
+    val withHeader = reader.allWithHeaders()
+    val classed = withHeader.map { row =>
+      // print(row.keySet)
+      if (
+        row.keySet != Set(
+          "TIME_PERIOD",
+          "firstname",
+          "YEAROFBIRTH",
+          "VALUE",
+          "OBS_STATUS"
+        )
+      ) {
+        // println("Error: CSV file does not have the expected header")
+        // println(row.keySet)
+        FemaleName("", "", 0, 0, "")
+      } else
+        FemaleName(
+          row("TIME_PERIOD"),
+          row("firstname"),
+          row("YEAROFBIRTH").toInt,
+          row("VALUE").toInt,
+          row("OBS_STATUS")
+        )
+    }
+    classed
   }
-  classed
-}
 
-def countSyllables(word: String): Int = {
-  val vowels = "aeiouyAEIOUY"
-  val regex = "[aeiouyAEIOUY]+".r
-  val syllables = regex.findAllIn(word).length
-  // if (word.endsWith("e")) syllables - 1 else syllables
-  syllables
-}
+  def countSyllables(word: String): Int = {
+    val vowels = "aeiouyAEIOUY"
+    val regex = "[aeiouyAEIOUY]+".r
+    val syllables = regex.findAllIn(word).length
+    // if (word.endsWith("e")) syllables - 1 else syllables
+    syllables
+  }
 
-def isOneEditAway(s1: String, s2: String): Boolean = {
-  val diff = s1.zip(s2).count { case (c1, c2) => c1 != c2 }
-  diff == 1 && s1.length == s2.length || Math.abs(s1.length - s2.length) == 1
+  def isOneEditAway(s1: String, s2: String): Boolean = {
+    val diff = s1.zip(s2).count { case (c1, c2) => c1 != c2 }
+    diff == 1 && s1.length == s2.length || Math.abs(s1.length - s2.length) == 1
+  }
 }
